077143ee2477fb0e00bfb7e4ff5377677509025c
Saqlain Hussain Shah <13719933+SaqlainHussainShah@users.noreply.github.com>
2020-03-09 16:19:22 +0500

    Add files via upload

--- user_Data_predict.py
+++ user_Data_predict.py
@@ -7,63 +7,68 @@ mymodel = load_model('my_model.h5')
 myvectorizer = pickle.load(open("vectorizer", 'rb'))
 
 
-beautify=''
-for i in range(20):
-        beautify+= "="
+def predict_sqli_attack():
+	
+	beautify=''
+	for i in range(20):
+		beautify+= "="
 
-print(beautify) 
-input_val=input("Give me some data to work on : ")
-print(beautify)
+	print(beautify) 
+	input_val=input("Give me some data to work on : ")
+	print(beautify)
 
-def clean_data(input_val):    
+	def clean_data(input_val):    
 
-    input_val=input_val.replace('\n', '')
-    input_val=input_val.replace('%20', ' ')
-    input_val=input_val.replace('=', ' = ')
-    input_val=input_val.replace('((', ' (( ')
-    input_val=input_val.replace('))', ' )) ')
-    input_val=input_val.replace('(', ' ( ')
-    input_val=input_val.replace(')', ' ) ')
-    input_val=input_val.replace('1 ', 'numeric')
-    input_val=input_val.replace(' 1', 'numeric')
-    input_val=input_val.replace("'1 ", "'numeric ")
-    input_val=input_val.replace(" 1'", " numeric'")
-    input_val=input_val.replace('1,', 'numeric,')
-    input_val=input_val.replace(" 2 ", " numeric ")
-    input_val=input_val.replace(' 3 ', ' numeric ')
-    input_val=input_val.replace(' 3--', ' numeric--')
-    input_val=input_val.replace(" 4 ", ' numeric ')
-    input_val=input_val.replace(" 5 ", ' numeric ')
-    input_val=input_val.replace(' 6 ', ' numeric ')
-    input_val=input_val.replace(" 7 ", ' numeric ')
-    input_val=input_val.replace(" 8 ", ' numeric ')
-    input_val=input_val.replace('1234', ' numeric ')
-    input_val=input_val.replace("22", ' numeric ')
-    input_val=input_val.replace(" 8 ", ' numeric ')
-    input_val=input_val.replace(" 200 ", ' numeric ')
-    input_val=input_val.replace("23 ", ' numeric ')
-    input_val=input_val.replace('"1', '"numeric')
-    input_val=input_val.replace('1"', '"numeric')
-    input_val=input_val.replace("7659", 'numeric')
-    input_val=input_val.replace(" 37 ", ' numeric ')
-    input_val=input_val.replace(" 45 ", ' numeric ')
-    
-    return input_val
+	    input_val=input_val.replace('\n', '')
+	    input_val=input_val.replace('%20', ' ')
+	    input_val=input_val.replace('=', ' = ')
+	    input_val=input_val.replace('((', ' (( ')
+	    input_val=input_val.replace('))', ' )) ')
+	    input_val=input_val.replace('(', ' ( ')
+	    input_val=input_val.replace(')', ' ) ')
+	    input_val=input_val.replace('1 ', 'numeric')
+	    input_val=input_val.replace(' 1', 'numeric')
+	    input_val=input_val.replace("'1 ", "'numeric ")
+	    input_val=input_val.replace(" 1'", " numeric'")
+	    input_val=input_val.replace('1,', 'numeric,')
+	    input_val=input_val.replace(" 2 ", " numeric ")
+	    input_val=input_val.replace(' 3 ', ' numeric ')
+	    input_val=input_val.replace(' 3--', ' numeric--')
+	    input_val=input_val.replace(" 4 ", ' numeric ')
+	    input_val=input_val.replace(" 5 ", ' numeric ')
+	    input_val=input_val.replace(' 6 ', ' numeric ')
+	    input_val=input_val.replace(" 7 ", ' numeric ')
+	    input_val=input_val.replace(" 8 ", ' numeric ')
+	    input_val=input_val.replace('1234', ' numeric ')
+	    input_val=input_val.replace("22", ' numeric ')
+	    input_val=input_val.replace(" 8 ", ' numeric ')
+	    input_val=input_val.replace(" 200 ", ' numeric ')
+	    input_val=input_val.replace("23 ", ' numeric ')
+	    input_val=input_val.replace('"1', '"numeric')
+	    input_val=input_val.replace('1"', '"numeric')
+	    input_val=input_val.replace("7659", 'numeric')
+	    input_val=input_val.replace(" 37 ", ' numeric ')
+	    input_val=input_val.replace(" 45 ", ' numeric ')
+	    
+	    return input_val
 
-input_val=clean_data(input_val)
-input_val=[input_val]
-
-
-
-input_val=myvectorizer.transform(input_val).toarray()
-
-result=mymodel.predict(input_val)
+	input_val=clean_data(input_val)
+	input_val=[input_val]
 
 
-print(beautify)
-if result>0.5:
-        print("ALERT :::: This can be SQL injection")
-elif result<=0.5:
-        print("It seems to be safe")
 
-print(beautify)
+	input_val=myvectorizer.transform(input_val).toarray()
+
+	result=mymodel.predict(input_val)
+
+
+	print(beautify)
+	if result>0.5:
+		print("ALERT :::: This can be SQL injection")
+	elif result<=0.5:
+		print("It seems to be safe")
+
+	print(beautify)
+
+
+predict_sqli_attack()
